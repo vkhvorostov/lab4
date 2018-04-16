3929f2267adfa3e6420b7a5f55c7eb845f6e7ddc
vkhvorostov <vladimir@khvorostov.ru>
2018-04-16 23:49:40 +0700

    Разместите необходимый html-код в файлах admin_header.php и admin_footer.php

---
+++ inc/admin_header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Система администрирования</title>
+    <style type="text/css">
+        table { border-collapse: collapse; }
+        table tr { border-bottom: 1px #aaa solid; }
+        table tr td { padding: 10px 8px 5px; }
+        table tr.deactivated td { background-color: #f0f0f0; }
+    </style>
+</head>
+<body>
